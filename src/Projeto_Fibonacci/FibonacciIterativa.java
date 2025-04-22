f693c1adf4afa2ec7e364f8d3bc185e3e54ac060
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-22 15:56:22 -0300

    Update FibonacciIterativa.java

--- src/Projeto_Fibonacci/FibonacciIterativa.java
+++ src/Projeto_Fibonacci/FibonacciIterativa.java
@@ -1,18 +1,19 @@
 package Projeto_Fibonacci;
 
-public class FibonacciIterativa {
-    public static int calculoInterativo(int n){
+public class FibonacciIterativa implements FibonacciStrategy {
+    public int calcular(int n){
         if (n == 0 || n == 1){
             return n;
         }
         int a = 0;
         int b = 1;
+        int soma = 0;
 
         for (int i = 2; i <= n; i++){
-            int soma = a + b;
+            soma = a + b;
             a = b;
             b = soma;
         }
-        return b;
+        return soma;
     }
 }
