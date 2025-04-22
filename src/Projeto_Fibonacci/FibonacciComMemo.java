4e8c132ec5d51b4e89613b1d891a489e9dd0479c
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-22 15:54:34 -0300

    Update FibonacciComMemo.java

--- src/Projeto_Fibonacci/FibonacciComMemo.java
+++ src/Projeto_Fibonacci/FibonacciComMemo.java
@@ -1,14 +1,26 @@
 package Projeto_Fibonacci;
 
-public class FibonacciComMemo {
-    public static int calculoMemo(int n, int[] memo){
+import java.util.Arrays;
+
+public class FibonacciComMemo implements FibonacciStrategy {
+    private int[] memo;
+
+    public int calcular(int n) {
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return calcularComMemo(n);
+    }
+
+    private int calcularComMemo(int n){
+
         if (memo[n] != -1){
             return memo[n];
         }
         if (n == 0 || n == 1){
             return n;
         }else {
-            return calculoMemo(n - 1, memo) + calculoMemo(n - 2, memo);
+            memo[n] = calcularComMemo(n - 1) + calcularComMemo(n - 2);
+            return memo[n];
         }
     }
 }
