ea016947ee886ce38c991e261daf51d9d4f77f95
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-22 14:49:28 -0300

    Create Fibonacci_Main.java

---
+++ src/Projeto_Fibonacci/Fibonacci_Main.java
@@ -0,0 +1,23 @@
+package Projeto_Fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci_Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continuar = true;
+
+        while (continuar){
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println(" ");
+
+
+
+        }
+    }
+}
