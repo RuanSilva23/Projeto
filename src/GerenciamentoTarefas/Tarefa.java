a806ae810c895ffee4be6e1dc4e992d2f86f2cca
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-29 23:24:37 -0300

    Update Tarefa.java

--- src/GerenciamentoTarefas/Tarefa.java
+++ src/GerenciamentoTarefas/Tarefa.java
@@ -1,4 +1,37 @@
 package GerenciamentoTarefas;
 
 public class Tarefa {
+    private int id;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(int id, String descricao){
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public boolean isConcluida(){
+        return concluida;
+    }
+
+    public void setDescricao(String NovaDescricao) {
+        this.descricao = NovaDescricao;
+    }
+
+    public void MarcarComoConcluida(){
+        this.concluida = true;
+    }
+
+    public String ToString(){
+        return String.format("[%d] %s - %s", id, descricao, (concluida ? "Concluida" : "Pendente") );
+    }
 }
