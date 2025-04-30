293ed7877c02e581549e5ea0b462662ca2196273
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-30 00:16:50 -0300

    Update GerenciadorTarefas.java

--- src/GerenciamentoTarefas/GerenciadorTarefas.java
+++ src/GerenciamentoTarefas/GerenciadorTarefas.java
@@ -1,4 +1,53 @@
 package GerenciamentoTarefas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorTarefas {
+    private List<Tarefa> tarefas = new ArrayList<>();
+    private int proximoid = 1;
+
+    public void adicionarTarefas(String descricao){
+        Tarefa nova = new Tarefa(proximoid++, descricao);
+        tarefas.add(nova);
+        System.out.println("Adicionado com sucesso!");
+    }
+
+    public void listarTarefas(){
+        if (tarefas.isEmpty()){
+            System.out.println("Nenhuma Tarefa foi encontrada. ");
+            return;
+        }
+
+        for (Tarefa t : tarefas){
+            System.out.println(t);
+        }
+    }
+
+    public void concluirTarefas(int id){
+        for (Tarefa t : tarefas){
+            if (t.getId() == id){
+                t.marcarComoConcluida();
+                System.out.println("Tarefa marcada como concluida.");
+                return;
+            }
+        }
+        System.out.println("Tarefa com Id" + id + " não encontrado. ");
+    }
+
+    public void removerTarefa(int id){
+        tarefas.removeIf(t -> t.getId() == id);
+        System.out.println("Tarefa removida. ");
+    }
+
+    public void editarTarefa(int id, String novaDescricao){
+        for (Tarefa t : tarefas){
+            if (t.getId() == id){
+                t.setDescricao(novaDescricao);
+                System.out.println("Descicao Atualizada. ");
+                return;
+            }
+        }
+        System.out.println("Tarefa com ID " + id + " não encontrada. ");
+    }
 }
