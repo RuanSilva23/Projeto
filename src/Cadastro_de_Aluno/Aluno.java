8055f22538f42a4d2eb6e63afdb45abd9e7ebf70
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-30 22:16:44 -0300

    Create Aluno.java

---
+++ src/Cadastro_de_Aluno/Aluno.java
@@ -0,0 +1,47 @@
+package Cadastro_de_Aluno;
+
+public class Aluno {
+    private int id;
+    private String aluno;
+    private int idade;
+    private String curso;
+
+    public Aluno(int id, String aluno, int idade, String curso){
+        this.id = id;
+        this.aluno = aluno;
+        this.idade = idade;
+        this.curso = curso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(String aluno) {
+        this.aluno = aluno;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String ToString(){
+        return String.format("ID: %d | Nome: %s| Idade: %d | Curso: %s", id, aluno, idade, curso);
+    }
+}
