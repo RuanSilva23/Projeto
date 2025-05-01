b7a7bdd9652578ed1a4989622c5ab204a46b623f
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-01 13:36:24 -0300

    Criei a classe mensagem para imprimir na tela se a condição for desfavorável

---
+++ src/Cadastro_de_Aluno/Mensagem.java
@@ -0,0 +1,14 @@
+package Cadastro_de_Aluno;
+
+public class Mensagem {
+    public static void alunoNaopodeVazio(){
+        System.out.println(" Erro.....");
+        System.out.println("O nome do aluno não pode ser vazio.");
+        System.out.println();
+    }
+
+    public static void idadeNaoNegativa(){
+        System.out.println("Erro!!!");
+        System.out.println("Idade não pode ser negativa.");
+    }
+}
