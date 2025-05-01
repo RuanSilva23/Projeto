25ee6a7350aaa7392b6856683e5dfba382fc970d
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-01 13:38:44 -0300

    Fiz um upgrade na exibição do menu para poder ficar mais legível e mais limpa também.

--- src/Cadastro_de_Aluno/ExibicaoDoMenu.java
+++ src/Cadastro_de_Aluno/ExibicaoDoMenu.java
@@ -3,15 +3,14 @@ package Cadastro_de_Aluno;
 import java.util.Scanner;
 
 public class ExibicaoDoMenu {
-    public static int exibirMenu( ){
-        Scanner scanner = new Scanner(System.in);
+    public static int exibirMenu(Scanner scanner ){
         System.out.println("------ Cadastro de Aluno -------- ");
         System.out.println("1 - Cadastro do Aluno ");
         System.out.println("2 - Consultar listar dos alunos ");
-        System.out.println("3 - Atualizar aluno ");
-        System.out.println("4 - Remover aluno ");
+        System.out.println("3 - Atualizar cadastro do aluno ");
+        System.out.println("4 - Remover cadastro do aluno ");
         System.out.println("5 - Sair ");
-        System.out.print("Qual opção escolhida: ");
+        System.out.print("Opção escolhida: ");
          return scanner.nextInt();
 
     }
