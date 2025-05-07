846236b1e8932f1d841ba31a27d45bc4562edf34
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-06 23:51:16 -0300

    Update TelaAdicionarAluno.java

--- src/Cadastro_de_Aluno/TelaAdicionarAluno.java
+++ src/Cadastro_de_Aluno/TelaAdicionarAluno.java
@@ -4,69 +4,91 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TelaAdicionarAluno extends JDialog {
+    private final JTextField campoNome = new JTextField();
+    private final JTextField campoIdade = new JTextField();
+    private final JTextField campoCurso = new JTextField();
+    private final CadastroAluno cadastro;
+
     public TelaAdicionarAluno(JFrame parent, CadastroAluno cadastro){
         super(parent, "Adicionar Aluno", true);
-        setSize(300,250);
+        this.cadastro = cadastro;
+
+        configurarJanela(parent);
+        inicializarComponentes();
+    }
+
+    //Configuração da estrutura Central da Tela
+    private void configurarJanela(JFrame parent) {
+        setSize(350, 300);
         setLocationRelativeTo(parent);
+        setLayout(new BorderLayout());
+    }
 
+    
+    private void inicializarComponentes() {
+        JLabel titulo = criarTitulo("Cadastro de Novo Aluno");
 
-        JPanel panel = new JPanel(new GridLayout(5,1,10,10));
+        JPanel painel = new JPanel(new GridLayout(7, 1, 10, 10));
+        painel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        painel.add(new JLabel("Nome do Aluno:"));
+        painel.add(campoNome);
 
+        painel.add(new JLabel("Idade do Aluno:"));
+        painel.add(campoIdade);
 
-        JLabel tituload = new JLabel("Cadastro de Novo Aluno", JLabel.CENTER);
-        tituload.setFont(new Font("Arial", Font.BOLD, 15));
-        tituload.setBorder(BorderFactory.createEmptyBorder(20,10,10,10));
+        painel.add(new JLabel("Curso do Aluno:"));
+        painel.add(campoCurso);
 
-        JTextField campoNome = new JTextField();
-        JTextField campoIdade = new JTextField();
-        JTextField campoCurso = new JTextField();
-
-
-        panel.add(new JLabel("Nome do Aluno: "));
-        panel.add(campoNome);
-
-        panel.add(new JLabel("Idade do Aluno: "));
-        panel.add(campoIdade);
-
-        panel.add(new JLabel("Curso do Aluno: "));
-        panel.add(campoCurso);
 
         JButton btnSalvar = new JButton("Salvar");
-        panel.add(btnSalvar);
+        btnSalvar.addActionListener(e -> salvarAluno());
+        painel.add(btnSalvar);
 
-        btnSalvar.addActionListener(e -> {
-            String nome = campoNome.getText().trim();
-            String idadeDoAluno = campoIdade.getText().trim();
-            String cursoDoAluno = campoCurso.getText().trim();
+        add(titulo, BorderLayout.NORTH);
+        add(painel, BorderLayout.CENTER);
+    }
 
-            if (nome.isEmpty() || idadeDoAluno.isEmpty() || cursoDoAluno.isEmpty()){
-                JOptionPane.showMessageDialog(this, "Todos os campos são obrigatórios.");
+    //Título
+    private JLabel criarTitulo(String texto){
+        JLabel titulo = new JLabel(texto, JLabel.CENTER);
+        titulo.setFont(new Font("Arial", Font.BOLD, 14));
+        titulo.setBorder(BorderFactory.createEmptyBorder(15,10,10,10));
+        return titulo;
+    }
+
+    //Salvação em arquivo
+    private void salvarAluno(){
+        String nome = campoNome.getText().trim();
+        String idadeAluno = campoIdade.getText().trim();
+        String curso = campoCurso.getText().trim();
+
+        if (nome.isEmpty() || idadeAluno.isEmpty() || curso.isEmpty()){
+            exibirMensagem("Todos os campos são obrigatórios");
+            return;
+        }
+
+
+        try {
+            int idade = Integer.parseInt(idadeAluno);
+
+            if (idade < 0){
+                exibirMensagem("Idade não pode ter valor negativo.");
                 return;
             }
 
-            try {
-                int idade = Integer.parseInt(idadeDoAluno);
-                if (idade < 0){
-                    JOptionPane.showMessageDialog(this,"Idade não pode ter valores negativa!!!");
-                    return;
-                }
 
-                cadastro.adicionarAluno(nome, idade, cursoDoAluno);
-                JOptionPane.showMessageDialog(this,"Aluno cadastrado com sucesso!!");
-                dispose();
-            }catch (NumberFormatException ex){
-                JOptionPane.showMessageDialog(this, "Idade inválida. Digite um número.");
-            }
-
-        });
-
-        setLayout(new BorderLayout());
-        add(tituload, BorderLayout.NORTH);
-        add(panel, BorderLayout.CENTER);
-
-
-        add(panel);
+            cadastro.adicionarAluno(nome, idade, curso);
+            exibirMensagem("Aluno cadastrado com sucesso.");
+            dispose();
+        }catch (NumberFormatException ex){
+            exibirMensagem("Idade inválida. Digite um número válido.");
+        }
     }
+
+
+    private void exibirMensagem(String mensagem){
+        JOptionPane.showMessageDialog(this, mensagem);
+    }
+
 }
