095c10f6755bac5f8a61d79acad657fd819cf570
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-07 23:39:09 -0300

    Update TelaRemoverAluno.java

--- src/Cadastro_de_Aluno/TelaRemoverAluno.java
+++ src/Cadastro_de_Aluno/TelaRemoverAluno.java
@@ -4,44 +4,59 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TelaRemoverAluno extends JDialog {
-    public TelaRemoverAluno(JFrame parent, CadastroAluno cadastro){
+    private final JTextField campoId = new JTextField();
+    private final CadastroAluno cadastro;
+
+    public TelaRemoverAluno(JFrame parent, CadastroAluno cadastro) {
         super(parent, "Remover Aluno", true);
-        setSize(300,200);
+        this.cadastro = cadastro;
+
+        configurarJanela(parent);
+        inializarComponentes();
+    }
+
+
+    private void configurarJanela(JFrame parent) {
+        setSize(300, 200);
         setLocationRelativeTo(parent);
+        setLayout(new BorderLayout());
 
-        JPanel panel = new JPanel(new GridLayout(3,1,10,10));
+    }
 
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+    private void inializarComponentes() {
 
         JLabel tituloremover = new JLabel("Exclusão de Cadastro de Aluno", JLabel.CENTER);
         tituloremover.setFont(new Font("Arial", Font.BOLD, 13));
-        tituloremover.setBorder(BorderFactory.createEmptyBorder(15,10,5,10));
+        tituloremover.setBorder(BorderFactory.createEmptyBorder(15, 10, 5, 10));
 
-        JTextField campoId = new JTextField();
+        JPanel painel = new JPanel(new GridLayout(3, 1, 10, 10));
 
-        panel.add(new JLabel("Digite o ID a qual será Removido: "));
-        panel.add(campoId);
+        painel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+
+        painel.add(new JLabel("Digite o ID a qual será Removido: "));
+        painel.add(campoId);
 
 
         JButton btnRemover = new JButton("Remover");
-        panel.add(btnRemover);
+        btnRemover.addActionListener(e -> removerAluno());
+        painel.add(btnRemover);
 
-        add(panel);
 
-        btnRemover.addActionListener(e -> {
-            try{
-                int id = Integer.parseInt(campoId.getText().trim());
-                cadastro.deleteAluno(id);
-                JOptionPane.showMessageDialog(this, "Se o aluno existia, ele foi removido.");
-                dispose();
-            }catch (NumberFormatException ex){
-                JOptionPane.showMessageDialog(this,"Digite um número de ID válido.");
-            }
-        });
-
-        setLayout(new BorderLayout());
         add(tituloremover, BorderLayout.NORTH);
-        add(panel, BorderLayout.CENTER);
+        add(painel, BorderLayout.CENTER);
+    }
 
+
+    private void removerAluno(){
+        try{
+            int id = Integer.parseInt(campoId.getText().trim());
+            cadastro.deleteAluno(id);
+            JOptionPane.showMessageDialog(this, "Se o aluno existia, ele foi removido.");
+            dispose();
+        }catch (NumberFormatException ex){
+                JOptionPane.showMessageDialog(this,"Digite um número de ID válido.");
+        }
     }
 }
