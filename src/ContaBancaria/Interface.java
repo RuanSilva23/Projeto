07e5b56c2863cf5f228c727666fcd2d4550d25e2
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-10 23:37:13 -0300

    Fazendo um upgrade nessa pasta ContaBancaria

---
+++ src/ContaBancaria/Interface.java
@@ -0,0 +1,11 @@
+package ContaBancaria;
+
+import java.util.Scanner;
+
+public class Interface {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual é o seu nome? ");
+        String nome = scanner.nextLine();
+    }
+}
