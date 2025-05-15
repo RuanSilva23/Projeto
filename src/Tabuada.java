8a8c3ba7652a11fbf1f9a0b405d2e28111d84cd3
Ruan Silva <ruan.silva2002@gmail.com>
2025-05-14 22:08:26 -0300

    Update Tabuada.java

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -3,18 +3,51 @@ import java.util.Scanner;
 public class Tabuada {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        boolean continua = true;
 
-        System.out.println("\n--------Tabuada---------");
+        while(continua) {
+            System.out.println("\n--------Tabuada---------");
+
+            int x = selecionadordaTabuada(scanner);
+
+            int y = selecionadorAteQuanto(scanner);
+
+            int i = 1;
+            do {
+                int resultado = x * i;
+                System.out.println(x + " * " + i + " = " + resultado);
+                i++;
+            } while (i <= y);
+            int continuar = continuarouNao(scanner);
+            if (continuar == 1){
+                continue;
+            }else{
+                continua = false;
+            }
+        }
+    }
+
+    public static int selecionadordaTabuada(Scanner scanner){
         System.out.print("Qual tabuada: ");
-        int x = scanner.nextInt();
-        System.out.print("Quantas vezes: ");
-        int y = scanner.nextInt();
+        return scanner.nextInt();
+    }
 
-        int i = 0;
-        do {
-            int resultado = x * i;
-            System.out.println(x + " * " + i + " = " + resultado);
-            i++;
-        }while (i <= y);
+    public static int selecionadorAteQuanto(Scanner scanner){
+        System.out.print("Quantas vezes: ");
+        return scanner.nextInt();
+    }
+
+    public static int continuarouNao(Scanner scanner){
+        System.out.print("Quer continuar(1) ou não(0): ");
+        int pergunta = scanner.nextInt();
+        System.out.println();
+        if (pergunta == 0 || pergunta == 1){
+            return pergunta;
+        }else {
+            System.out.print("Opção inválida!! Tente novamente.");
+            System.out.println();
+            return continuarouNao(scanner);
+
+        }
     }
 }
